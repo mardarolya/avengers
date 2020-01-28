435d1f5e702c2bc5c60d706814a4ab1404dade1e
Olha Mardar <omskazka@gmail.com>
2020-01-28 14:03:16 +0200

    Updates

---
+++ precache-manifest.d98a4ab659c11f788b07a2786fd98b7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6029731c62bcf664442fcf9829ea3a23",
+    "url": "/avengers/index.html"
+  },
+  {
+    "revision": "a3a3e60de9b723c16e2b",
+    "url": "/avengers/static/css/main.470d02cd.chunk.css"
+  },
+  {
+    "revision": "04e035d9b449f7b2b1cf",
+    "url": "/avengers/static/js/2.88e8cedb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/avengers/static/js/2.88e8cedb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3a3e60de9b723c16e2b",
+    "url": "/avengers/static/js/main.c74c5306.chunk.js"
+  },
+  {
+    "revision": "f46809a7447de9f6cef8",
+    "url": "/avengers/static/js/runtime-main.03c8cd5b.js"
+  },
+  {
+    "revision": "c78762abb0707eef6b153aa8f565f50b",
+    "url": "/avengers/static/media/logo.c78762ab.png"
+  }
+]);
